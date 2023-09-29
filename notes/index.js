93445584ca143afd61b3a746dca1a1ec6bd368a7
azatshtru <adwaitarajaria@Adwaits-MacBook-Pro.local>
2023-09-29 23:52:58 +0530

    fixed css layout issues in note taking

--- notes/index.js
+++ notes/index.js
@@ -22,6 +22,11 @@ markdownSubmitButton.addEventListener('click', () => {
     const htmlString = parseMarkdown(markdownTextarea.value);
     markdownRenderBox.parentElement.style.display = 'initial';
     markdownRenderBox.innerHTML = htmlString;
+
+    const footer = document.createElement('div');
+    footer.style.height = '1in';
+    markdownRenderBox.appendChild(footer);
+
     markdownTextarea.parentElement.style.display = 'none';
 });
 
@@ -34,3 +39,25 @@ const markdownRenderCloseButton = document.querySelector('#markdown-render-close
 markdownRenderCloseButton.addEventListener('click', () => {
     markdownRenderBox.parentElement.style.display = 'none';
 });
+
+/* 
+# nothing will begin unless I start writing this shit out.
+
+I am trying to type fast and believe me I am but fuck this shit taking too long to even begin being functional.
+
+ok lets try to reach the end of the page.
+
+I will have to type something really filler up.
+
+I can't seem to think anything.
+# here's a heading
+# here's another
+
+I wonder if two headings one below the other looks appealing or not.
+
+but fuck this shit anything to make it to the end of the page like it's a real fucking piece of text. And yep I sweared into a note but man this is taking a toll on me.
+here's one more filler heading.
+
+and here we go at the end of the fucking page.
+and for the most part this seems to be working, only I can't see the upper part so. 
+*/
