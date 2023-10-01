b6fb8526d24033837e3f1127204bd04e7905f3bb
azatshtru <adeveloper121@gmail.com>
2023-10-02 00:37:18 +0530

    added tags
    users can use the '@' symbol to add tags to their content dynamically, tags will automatically show up on sides and will be removed if all their instances from notes are removed.

--- notes/index.js
+++ notes/index.js
@@ -3,24 +3,41 @@ let notesDataObjectModel = {
         {
             content: `# Hi guys
 kaise`,
+            tags: [],
         },
         {
             content: "Hello guys",
+            tags: [],
         },
         {
             content: "# here is some content",
+            tags: [],
         },
     ],
-    tags: []
+    tags: {},
 }
 
 const getTitle = (x) => {
-    if (x.content.trim() == '') { return 'untitled' }
-    return x.content.match(/[\w\d].*/gim)[0];
+    if (x.trim() == '') { return 'untitled' }
+    return x.match(/[\w\d].*/gim)[0];
 }
 
-const notesContainer = document.querySelector('#notes-container');
-notesDataObjectModel.notes.forEach((x, i) => {
+const removeTagIfEmpty = (x) => {
+    if (notesDataObjectModel.tags[x].length == 0) {
+        delete notesDataObjectModel.tags[x];
+        document.querySelector(`[data-tagname="${x}"]`).remove();
+    }
+}
+
+function tagChip(content){
+    const chip = document.createElement('button');
+    chip.className = 'chip';
+    chip.textContent = content;
+    chip.dataset.tagname = content;
+    return chip;
+}
+
+function tonalNoteButton(content){
     const noteButton = document.createElement('div');
     noteButton.classList.add('note-button', 'tonal-button');
 
@@ -29,15 +46,26 @@ notesDataObjectModel.notes.forEach((x, i) => {
     noteIcon.textContent = 'notes';
 
     noteButton.appendChild(noteIcon);
-    const noteTitle = document.createTextNode(getTitle(x));
+    const noteTitle = document.createTextNode(content);
     noteButton.appendChild(noteTitle);
 
-    noteButton.dataset.noteIndex = i;
+    return noteButton;
+}
 
+const tagsContainer = document.querySelector('#tags-container');
+Object.keys(notesDataObjectModel.tags)?.forEach(x => {
+    tagsContainer.appendChild(tagChip(x));
+});
+
+const notesContainer = document.querySelector('#notes-container');
+notesDataObjectModel.notes?.forEach((x, i) => {
+    const noteButton = tonalNoteButton(getTitle(x.content));
+    noteButton.dataset.noteIndex = i;
     notesContainer.appendChild(noteButton);
 });
 
 let currentActiveNote;
+let currentActiveTaglist;
 
 const markdownRenderBox = document.querySelector('#markdown-render-box');
 const markdownEditButton = document.querySelector('#markdown-edit-button');
@@ -65,10 +93,31 @@ markdownEditButton.addEventListener('click', () => {
 });
 
 markdownSubmitButton.addEventListener('click', () => {
+    currentActiveTaglist.forEach(x => {
+        notesDataObjectModel.tags[x] = notesDataObjectModel.tags[x].filter(item => item !== currentActiveNote);
+        removeTagIfEmpty(x);
+    });
+    notesDataObjectModel.notes[currentActiveNote].tags = [];
+    const editedActiveTaglist = markdownTextarea.value.match(tagRegex);
+
+    editedActiveTaglist?.forEach(x => {
+        const currentTag = x.slice(1).trim();
+        notesDataObjectModel.notes[currentActiveNote].tags.push(currentTag);
+        if (!(currentTag in notesDataObjectModel.tags)) { 
+            notesDataObjectModel.tags[currentTag] = [currentActiveNote];
+            tagsContainer.appendChild(tagChip(currentTag));
+        } else {
+            notesDataObjectModel.tags[currentTag].push(currentActiveNote);
+        }
+    });
+
     const htmlString = parseMarkdown(markdownTextarea.value);
     notesDataObjectModel.notes[currentActiveNote].content = markdownTextarea.value;
+    document.querySelector(`[data-note-index="${currentActiveNote}"]`).lastChild.textContent = getTitle(markdownTextarea.value);
     markdownRenderBox.parentElement.style.display = 'initial';
     markdownRenderBox.innerHTML = htmlString;
+
+    //TODO: Update data in firestore.
 
     const footer = document.createElement('div');
     footer.style.height = '1in';
@@ -81,6 +130,7 @@ const noteButtons = document.querySelectorAll('.note-button');
 noteButtons.forEach(x => x.addEventListener('click', () => {
     markdownRenderBox.parentElement.style.display = 'initial';
     currentActiveNote = x.dataset.noteIndex;
+    currentActiveTaglist = notesDataObjectModel.notes[currentActiveNote].tags;
     markdownRenderBox.innerHTML = parseMarkdown(notesDataObjectModel.notes[currentActiveNote].content);
 }));
 
